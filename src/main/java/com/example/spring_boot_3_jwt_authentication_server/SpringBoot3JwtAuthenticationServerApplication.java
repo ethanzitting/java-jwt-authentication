5ae9328d4685d3f1065fd83a828dd1650af4dd40
Ethan Zitting <ethan@ethanzitting.com>
2024-05-24 08:14:19 -0500

    initial commit

---
+++ src/main/java/com/example/spring_boot_3_jwt_authentication_server/SpringBoot3JwtAuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_jwt_authentication_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtAuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3JwtAuthenticationServerApplication.class, args);
+	}
+
+}
